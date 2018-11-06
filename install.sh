3bb44a6dff48b299a4f2778043cc9f6e08fa16d9
Adam Montgomery <adam@amontgomery1.lab.novell.com>
2018-11-06 13:00:40 -0700

    Added some info to the readme and created an installer script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#installer script
+
+if ! [ $(id -u) = 0 ]; then
+   echo "This script needs to be run with root privileges." >&2
+   exit 1
+fi
+
+echo "installing sc tool to /usr/local/bin"
+
+for file in boot.mod updates.mod sc
+do cp $file /usr/local/bin/$file
+done
