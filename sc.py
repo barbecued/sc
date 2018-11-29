612168dbe3eda73b213efc98baaef5511c4dc752
ch2304 <cwhazzoo@gmail.com>
2018-11-29 13:10:37 -0700

    Prints os version

--- sc.py
+++ sc.py
@@ -1,2 +1,19 @@
+#! /usr/bin/python3
 #SC Python Script
 #Use to help anaylize supportconfig files
+
+import sys
+import argparse
+import re
+
+parser = argparse.ArgumentParser('sc.py')
+parser.add_argument('-v', action='store_true', help='print verbose output')
+parser.add_argument('-p', action='store_true', help='print information about service performance (i.e. # of cpus, sar data, etc)')
+
+args = parser.parse_args()
+
+#Display OS Version
+text2find = 'PRETTY_NAME'
+for line in open('basic-environment.txt', 'r'):
+    if re.search(text2find, line):
+        print('OS Version: ' + re.findall(r'"(.*?)"', line)[0])
